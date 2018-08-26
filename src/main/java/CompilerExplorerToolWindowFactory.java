1f2408211c754d4fafad2b1a6f6cb544bdb2663b
Oleksandr Grebenyuk <grebenyukoleksandr@gmail.com>
2018-08-26 11:16:51 -0500

    Add tool window

---
+++ src/main/java/CompilerExplorerToolWindowFactory.java
@@ -0,0 +1,10 @@
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class CompilerExplorerToolWindowFactory implements ToolWindowFactory {
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+}
