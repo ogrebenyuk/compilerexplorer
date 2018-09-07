0a48f16560eb71187ea0c79915dd6b850c06acda
Oleksandr Grebenyuk <grebenyukoleksandr@gmail.com>
2018-09-06 19:48:23 -0500

    Process the compiler's stdout and stderr the right way to prevent hangs

--- src/main/java/com/compilerexplorer/compiler/common/CompilerRunner.java
+++ src/main/java/com/compilerexplorer/compiler/common/CompilerRunner.java
@@ -2,9 +2,9 @@ package com.compilerexplorer.compiler.common;
 
 import com.intellij.openapi.progress.ProgressIndicator;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
-import java.util.concurrent.TimeUnit;
 
 public class CompilerRunner {
     @NotNull
@@ -15,17 +15,40 @@ public class CompilerRunner {
 
     public CompilerRunner(@NotNull String[] commandArray, @NotNull File workingDir, @NotNull String stdin, @NotNull ProgressIndicator progressIndicator) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime().exec(commandArray, null, workingDir);
-        OutputStream stdinStream = process.getOutputStream();
-        stdinStream.write(stdin.getBytes());
-        stdinStream.flush();
-        stdinStream.close();
-        stdout = read(process.getInputStream(), progressIndicator);
-        stderr = read(process.getErrorStream(), progressIndicator);
-        while (!process.waitFor(100, TimeUnit.MILLISECONDS)) {
+
+        try {
+            OutputStream stdinStream = process.getOutputStream();
+            stdinStream.write(stdin.getBytes());
+            stdinStream.flush();
+            stdinStream.close();
+
+            StringBuilder stdoutBuilder = new StringBuilder();
+            StringBuilder stderrBuilder = new StringBuilder();
+            StreamGobbler stdoutGobbler = new StreamGobbler(process.getInputStream(), stdoutBuilder, progressIndicator);
+            StreamGobbler stderrGobbler = new StreamGobbler(process.getErrorStream(), stderrBuilder, progressIndicator);
+            Thread stdoutGobblerThread = new Thread(stdoutGobbler);
+            Thread stderrGobblerThread = new Thread(stderrGobbler);
+            stdoutGobblerThread.start();
+            stderrGobblerThread.start();
+
+            exitCode = process.waitFor();
+            stdoutGobblerThread.join();
+            stderrGobblerThread.join();
+
+            if (stdoutGobbler.getException() != null) {
+                throw new InterruptedException(stdoutGobbler.getException().getMessage());
+            }
+            if (stderrGobbler.getException() != null) {
+                throw new InterruptedException(stderrGobbler.getException().getMessage());
+            }
+
             progressIndicator.checkCanceled();
+            stdout = stdoutBuilder.toString().replaceAll("\r\n?", "\n");
+            stderr = stderrBuilder.toString().replaceAll("\r\n?", "\n");
+        } catch (Exception exception) {
+            process.destroyForcibly();
+            throw exception;
         }
-        exitCode = process.exitValue();
-        progressIndicator.checkCanceled();
     }
 
     @NotNull
@@ -42,17 +65,37 @@ public class CompilerRunner {
         return exitCode;
     }
 
-    @NotNull
-    private static String read(InputStream stream, @NotNull ProgressIndicator progressIndicator) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        String result = "";
-        String line;
-        while ((line = reader.readLine()) != null) {
-            result = result.concat(line).concat("\n");
-            progressIndicator.checkCanceled();
+    private static class StreamGobbler implements Runnable {
+        @NotNull
+        private final InputStream inputStream;
+        @NotNull
+        private final StringBuilder stringBuilder;
+        @NotNull
+        private final ProgressIndicator progressIndicator;
+        @Nullable
+        private Exception exception;
+
+        StreamGobbler(@NotNull InputStream inputStream_, @NotNull StringBuilder stringBuilder_, @NotNull ProgressIndicator progressIndicator_) {
+            inputStream = inputStream_;
+            stringBuilder = stringBuilder_;
+            progressIndicator = progressIndicator_;
         }
-        reader.close();
-        progressIndicator.checkCanceled();
-        return result;
+
+        public void run() {
+            try {
+                int c;
+                while ((c = inputStream.read()) != -1) {
+                    stringBuilder.append((char) c);
+                    progressIndicator.checkCanceled();
+                }
+            } catch (IOException x) {
+                exception = x;
+            }
+        }
+
+        @Nullable
+        Exception getException() {
+            return exception;
+        }
     }
 }
